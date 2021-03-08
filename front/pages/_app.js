b7dc57ea79975e0e7a0f464262150a205b392123
812jay <yongje812@gmail.com>
2021-03-09 01:11:33 +0900

    Implement Of Redux

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,13 +1,14 @@
 import React from "react";
-import PropTypes from "prop-types";
 import Head from "next/head";
+import PropTypes from "prop-types";
 import "antd/dist/antd.css";
+
+import wrapper from "../store/configureStore";
 
 const NodeBird = ({ Component }) => {
   return (
     <>
       <Head>
-        <meta charSet="utf-8" />
         <title>NodeBird</title>
       </Head>
       <Component />
@@ -15,8 +16,8 @@ const NodeBird = ({ Component }) => {
   );
 };
 
-NodeBird.proptypes = {
+NodeBird.propTypes = {
   Component: PropTypes.elementType.isRequired,
 };
 
-export default NodeBird;
+export default wrapper.withRedux(NodeBird);
