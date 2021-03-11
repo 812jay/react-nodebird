750e560cf0aaa9be12304e7a0450e7b493d0eaac
812jay <yongje812@gmail.com>
2021-03-11 21:58:26 +0900

    Implementing a Post

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -9,13 +9,14 @@ export const initialState = {
       content: "첫번째 게시글 #해시태그 #익스프레스",
       Images: [
         {
-          src: "https://unsplash.com/photos/YNliXm_hMn8",
+          src:
+            "https://bookthumb-phinf.pstatic.net/cover/137/995/13799585.jpg?udate=20180726",
         },
         {
-          src: "https://unsplash.com/photos/p47oVX9qhkA",
+          src: "https://gimg.gilbut.co.kr/book/BN001958/rn_view_BN001958.jpg",
         },
         {
-          src: "https://unsplash.com/photos/zw8VDs_Pzk8",
+          src: "https://gimg.gilbut.co.kr/book/BN001998/rn_view_BN001998.jpg",
         },
       ],
       Comments: [
@@ -33,12 +34,19 @@ export const initialState = {
 };
 
 const ADD_POST = "ADD_POST";
+
 export const addPost = {
   type: ADD_POST,
 };
 const dummyPost = {
   id: 2,
   content: "더미",
+  User: {
+    id: 1,
+    nickname: "제로초",
+  },
+  Images: [],
+  Comments: [],
 };
 
 const reducer = (state = initialState, action) => {
