b4ede81aa124e5336c42ff1ab642cd731654a546
812jay <yongje812@gmail.com>
2021-03-17 23:47:39 +0900

    Create Dummy data With Faker

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,13 +1,21 @@
-import React from "react";
-import Head from "next/head";
-import { useSelector } from "react-redux";
-
-import AppLayout from "../components/AppLayout";
-import NicknameEditForm from "../components/NicknameEditForm";
-import FollowList from "../components/FollowList";
+import React, { useEffect } from 'react';
+import Head from 'next/head';
+import { useSelector } from 'react-redux';
+import Router from 'next/router';
+import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
 
 const Profile = () => {
-  const {me} = useSelector(state => state.user); 
+  const { me } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (!(me && me.id)) {
+      Router.push('/');
+    }
+  }, [me && me.id]);
+  if (!me) {
+    return null;
+  }
   return (
     <>
       <Head>
