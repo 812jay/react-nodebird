ae38124edc0bfe4c2f234cec263420fb24defc33
812jay <yongje812@gmail.com>
2021-03-20 14:22:32 +0900

    Routing to Express

--- back/app.js
+++ back/app.js
@@ -1,12 +1,31 @@
-const http = require('http');
-const server = http.createServer((req, res)=>{
-    console.log(req.url, req.method);
-    res.write('<h1>Hello Node1</h1>');
-    res.write('<h2>Hello Node2</h2>');
-    res.write('<h3>Hello Node3</h3>');
-    res.write('<h4>Hello Node4</h4>');
-    res.end('<h5>Hello Node5</h5>');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello Express');
 });
-server.listen(3065, () => {
-    console.log('콘솔 실행중.')
+
+app.get('/', (req, res) => {
+    res.send('Hello API');
+});
+
+app.get('/api/posts', (req, res) => {
+    res.json([
+        {id: 1, content: 'hello'},
+        {id: 2, content: 'hello2'},
+        {id: 3, content: 'hello3'}
+    ]);
+});
+
+app.post('/api/post', (req, res) => {
+    res.json({ id: 1, content: 'hello' });
+});
+
+app.delete('/api/post', (req, res) => {
+    res.json({ id: 1 });
+});
+
+app.listen(3065, () => {
+    console.log('서버 실행중.')
 });
