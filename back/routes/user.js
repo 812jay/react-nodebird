aae648354bdabbc54eea3fff0d2e78839b771a49
812jay <yongje812@gmail.com>
2021-03-23 14:44:27 +0900

    Troubleshooting The CORS issue

--- back/routes/user.js
+++ back/routes/user.js
@@ -6,14 +6,14 @@ const router = express.Router();
 
 router.post('/', async (req, res, next) => {
     try {
-        const exUser = User.findOne({
+        const exUser = await User.findOne({
            where: {
                email: req.body.email,
            } 
         });
         if(exUser){
             console.log(`exUser! ${JSON.stringify(exUser)}`)
-            return res.status(403).send('이미 사용중인 아이디 압니다.');
+            return res.status(403).send('이미 사용 중인 아이디입니다.');
         }
         const hashedPassword = await bcrypt.hash(req.body.password, 11);
         await User.create({
